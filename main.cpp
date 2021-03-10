e903bfd6f5d66a638e27d319fbf3985d7e78d928
Matth-ewe-f <matthewtf73@gmail.com>
2021-03-09 23:59:19 -0500

    added main ccp and h files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Matthew Flynn and Andrew Suh
+// CSF Assignment #3
+
+#include <iostream>
+
+using std::cerr;
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[]) {
+    if (argc < 7) {
+        cerr << "Not enough arguments" << endl;
+    }
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+// Matthew Flynn and Andrew Suh
+// CSF Assignment #3
+
+#ifndef CACHE_SIM
+#define CACHE_SIM
+
+#endif
