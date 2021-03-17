b8b8e1560b30f2adea8b7e32889f69a9e58f5dbf
Matth-ewe-f <matthewtf73@gmail.com>
2021-03-16 22:31:05 -0400

    reindented file to be consistent

--- Cache.cpp
+++ Cache.cpp
@@ -10,90 +10,96 @@ using std::map;
 using std::vector;
 
 Cache::Cache (int numSets, int numBlocksInSet, int numBytesInBlock, bool writeAllocate, bool writeThrough, bool lru) {
-  // initialize counters
-  this->loads = 0;
-  this->stores = 0;
-  this->loadHits = 0;
-  this->loadMisses = 0;
-  this->storeHits = 0;
-  this->storeMisses = 0;
-  this->cycles = 0;
+    // initialize counters
+    this->loads = 0;
+    this->stores = 0;
+    this->loadHits = 0;
+    this->loadMisses = 0;
+    this->storeHits = 0;
+    this->storeMisses = 0;
+    this->cycles = 0;
 
-  // initialize Cache configurations
-  this->numSets = numSets;
-  this->numBlocksInSet = numBlocksInSet;
-  this->numBytesInBlock = numBytesInBlock;
-  this->writeAllocate = writeAllocate;
-  this->writeThrough = writeThrough;
-  this->lru = lru;
-  
-  // initialize map used to keep track of cache blocks
-  sets = new map<int, vector<CacheBlock>>;
+    // initialize Cache configurations
+    this->numSets = numSets;
+    this->numBlocksInSet = numBlocksInSet;
+    this->numBytesInBlock = numBytesInBlock;
+    this->writeAllocate = writeAllocate;
+    this->writeThrough = writeThrough;
+    this->lru = lru;
+    
+    // initialize map used to keep track of cache blocks
+    sets = new map<int, vector<CacheBlock>>;
 
-  // print initialization settings
-  cout << "initialized cache with:" << endl;
-  cout << "\t" << numSets << " sets\n"
-       << "\t" << numBlocksInSet << " blocks per set\n"
-       << "\t" << numBytesInBlock << " bytes in block\n"
-       << "\t" << (writeAllocate ? "write-allocate" : "no-write-allocate") << "\n"
-       << "\t" << (writeThrough ? "write-through" : "write-back") << "\n"
-       << "\t" << (lru == true ? "lru" : "fifo") << endl;
+    // print initialization settings
+    cout << "initialized cache with:" << endl;
+    cout << "\t" << numSets << " sets\n"
+             << "\t" << numBlocksInSet << " blocks per set\n"
+             << "\t" << numBytesInBlock << " bytes in block\n"
+             << "\t" << (writeAllocate ? "write-allocate" : "no-write-allocate") << "\n"
+             << "\t" << (writeThrough ? "write-through" : "write-back") << "\n"
+             << "\t" << (lru == true ? "lru" : "fifo") << endl;
 }
 
 Cache::~Cache() {
-  delete sets;
+    delete sets;
 }
 
 int Cache::getAddressIndex(int fullAddress) {
-  // bit shifting discards offset, mod discards tag
-  return (fullAddress >> (numBytesInBlock * 8)) % numSets;
+    // bit shifting discards offset, mod discards tag
+    return (fullAddress >> (numBytesInBlock * 8)) % numSets;
 }
 
 int Cache::getAddressTag(int fullAddress) {
-  // bit shifting discards offset, division discards index
-  return (fullAddress >> (numBytesInBlock * 8)) / numSets;
+    // bit shifting discards offset, division discards index
+    return (fullAddress >> (numBytesInBlock * 8)) / numSets;
 }
 
 void Cache::performLoad(int address) {
-  // get relevant pieces of address
-  int index = getAddressIndex(address);
-  int tag = getAddressTag(address);
+    loads++;
+    // get relevant pieces of address
+    int index = getAddressIndex(address);
+    int tag = getAddressTag(address);
 
-  // search the cache for the requested block
-  vector<CacheBlock> set = sets->at(index);
-  for (vector<CacheBlock>::iterator it = set.begin();
-      it != set.end();
-      it++) {
-    if (tag == it->getTag()) {
-      // TODO cache hit has occured
+    // search the cache for the requested block
+    vector<CacheBlock> set = sets->at(index);
+    for (vector<CacheBlock>::iterator it = set.begin();
+            it != set.end();
+            it++) {
+        if (tag == it->getTag()) {
+            loadHits++;
+            return;
+        }
     }
-  }
-  // TODO cache miss has occured
+    loadMisses++;
+    // TODO cache miss has occured
 }
 
 void Cache::performStore(int address) {
-  // get relevant pieces of address
-  int index = getAddressIndex(address);
-  int tag = getAddressTag(address);
+    stores++;
+    // get relevant pieces of address
+    int index = getAddressIndex(address);
+    int tag = getAddressTag(address);
 
-  // search the cache for the requested block
-  vector<CacheBlock> set = sets->at(index);
-  for (vector<CacheBlock>::iterator it = set.begin();
-      it != set.end();
-      it++) {
-    if (tag == it->getTag()) {
-      // TODO cache hit has occured
+    // search the cache for the requested block
+    vector<CacheBlock> set = sets->at(index);
+    for (vector<CacheBlock>::iterator it = set.begin();
+            it != set.end();
+            it++) {
+        if (tag == it->getTag()) {
+            storeHits++;
+            // TODO cache hit has occured
+        }
     }
-  }
-  // TODO cache miss has occured
+    storeMisses++;
+    // TODO cache miss has occured
 }
 
 void Cache::printResults() {
-  cout << "Total loads: " << loads
-       << "\nTotal stores: " << stores
-       << "\nLoad hits: " << loadHits
-       << "\nLoad misses: " << loadMisses
-       << "\nStore hits: " << storeHits
-       << "\nStore misses: " << storeMisses
-       << "\nTotal cycles: " << cycles << endl;
+    cout << "Total loads: " << loads
+             << "\nTotal stores: " << stores
+             << "\nLoad hits: " << loadHits
+             << "\nLoad misses: " << loadMisses
+             << "\nStore hits: " << storeHits
+             << "\nStore misses: " << storeMisses
+             << "\nTotal cycles: " << cycles << endl;
 }
--- Cache.h
+++ Cache.h
@@ -6,33 +6,33 @@
 #include "CacheBlock.h"
 
 class Cache {
-  private:
-    // map to simulate cache
-    std::map<int, std::vector<CacheBlock>> *sets;
-    // tracking variables for performance reporting
-    int loads;
-    int stores;
-    int loadHits;
-    int loadMisses;
-    int storeHits;
-    int storeMisses;
-    int cycles;
-    // variables for cache settings
-    int numSets;
-    int numBlocksInSet;
-    int numBytesInBlock;
-    bool writeAllocate;
-    bool writeThrough;
-    bool lru;
-    // functions
-    int getAddressIndex(int fullAddress);
-    int getAddressTag(int fullAddress);
+    private:
+        // map to simulate cache
+        std::map<int, std::vector<CacheBlock>> *sets;
+        // tracking variables for performance reporting
+        int loads;
+        int stores;
+        int loadHits;
+        int loadMisses;
+        int storeHits;
+        int storeMisses;
+        int cycles;
+        // variables for cache settings
+        int numSets;
+        int numBlocksInSet;
+        int numBytesInBlock;
+        bool writeAllocate;
+        bool writeThrough;
+        bool lru;
+        // functions
+        int getAddressIndex(int fullAddress);
+        int getAddressTag(int fullAddress);
 
-  public:
-    Cache(int numSets, int numBlocksInSet, int numBytesInBlock, bool writeAllocate, bool writeThrough, bool lru);
-    void performLoad(int address);
-    void performStore(int address);
-    void printResults();
+    public:
+        Cache(int numSets, int numBlocksInSet, int numBytesInBlock, bool writeAllocate, bool writeThrough, bool lru);
+        void performLoad(int address);
+        void performStore(int address);
+        void printResults();
 };
 
 #endif
